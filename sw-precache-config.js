cf5b2e61d8f1f131944dc3dda7e4dea504421560
Daniel Romain <romain.daniel@epitech.eu>
2019-08-14 17:21:39 +0200

    init polymer2 starter kit

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
  * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
  * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
  * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
@@ -8,19 +8,13 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
+/* eslint-env node */
+
 module.exports = {
   staticFileGlobs: [
+    'index.html',
     'manifest.json',
-    'src/**/*',
+    'bower_components/webcomponentsjs/*',
   ],
-  runtimeCaching: [
-    {
-      urlPattern: /\/@webcomponents\/webcomponentsjs\//,
-      handler: 'fastest'
-    },
-    {
-      urlPattern: /^https:\/\/fonts.gstatic.com\//,
-      handler: 'fastest'
-    }
-  ]
+  navigateFallback: 'index.html',
 };
