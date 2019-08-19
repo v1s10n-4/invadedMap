6b0e07ca0d30357c3fc2b4ede1c8a5a1871b793b
Daniel Romain <romain.daniel@epitech.eu>
2019-08-19 16:18:56 +0200

    update favicons + manifest with icons + splash screens

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -14,10 +14,68 @@ module.exports = {
 	staticFileGlobs: [
 		'index.html',
 		'locations.json',
+		'images/**/*',
+		'images/*.svg',
 		'manifest.json',
 		'bower_components/webcomponentsjs/*',
 		'bower_components/google-map/*',
 		'bower_components/google-map-markerclusterer/*',
 	],
 	navigateFallback: 'index.html',
+	runtimeCaching: [
+		{
+			urlPattern: /\/src\/*.html/g,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'src/*-cache'
+				}
+			}
+		},
+		{
+			urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'webcomponentsjs-polyfills-cache'
+				}
+			}
+		},
+		{
+			urlPattern: /https:\/\/maps.gstatic.com\/mapfiles\/closedhand_8_8.cur/g,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'google-map-cursor'
+				}
+			}
+		},
+		{
+			urlPattern: /https:\/\/maps.googleapis.com\/maps\/vt?pb=*/g,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'google-map-cache'
+				}
+			}
+		},
+		{
+			urlPattern: /http:\/\/invader.spotter.free.fr\/images\/.*.png/g,
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'google-map-cache'
+				}
+			}
+		},
+		{
+			urlPattern: 'https://cdn.polyfill.io/v2/polyfill.js?features=fetch',
+			handler: 'fastest',
+			options: {
+				cache: {
+					name: 'fetch-polyfill'
+				}
+			}
+		}
+	],
 };
