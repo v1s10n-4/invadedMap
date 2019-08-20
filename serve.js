5fcd99c7074d5bd05956ddb751862e94be03f93e
Daniel Romain <romain.daniel@epitech.eu>
2019-08-20 19:28:18 +0200

    fix csp errors with images by storing them

--- serve.js
+++ serve.js
@@ -1,22 +1,29 @@
 /* global process require __dirname */
 const prpl = require('prpl-server'),
-    express = require('express'),
-    morgan = require('morgan'),
-    app = express(),
-    config = require('./build/polymer.json'),
-    compression = require('compression'),
-    httpsRedirect = require('express-https-redirect');
+	express = require('express'),
+	morgan = require('morgan'),
+	path = require('path'),
+	app = express(),
+	config = require('./build/polymer.json'),
+	compression = require('compression'),
+	httpsRedirect = require('express-https-redirect');
 
 
-console.info(process.env || 5002);
-if (process.env.ENV === 'dev' || process.env.ENV === 'staging') {
-    app.use(morgan('dev'));
-    app.use(compression());
-    app.use(express.static(`${__dirname}/`));
-} else {
-    app.use(compression());
-    app.set('view cache', true);
-    app.use('/', httpsRedirect());
-    app.get('/*', prpl.makeHandler('./build/', config));
-}
+console.info(process.env.PORT || 5002);
+app.use(compression());
+app.set('view cache', true);
+app.use('/', httpsRedirect());
+app.get('/images/invaders/:name', (req, res) => {
+	var options = {
+		root: path.join(__dirname, 'images/invaders'),
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	}
+	var fileName = req.params.name;
+	res.sendFile(fileName, options, err => err ? next(err) : console.log('Sent:', fileName));
+});
+app.get('/*', prpl.makeHandler('./build/', config));
 app.listen(process.env.PORT || 5002);
