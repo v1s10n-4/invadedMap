216d5851272e07ac250715c381d35bbb3d43d42f
Daniel Romain <romain.daniel@epitech.eu>
2019-09-10 14:13:26 +0200

    new colors theme + highscore page + css loader

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,13 +1,3 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
-
 /* eslint-env node */
 
 module.exports = {
@@ -39,6 +29,15 @@ module.exports = {
 			options: {
 				cache: {
 					name: 'webcomponentsjs-polyfills-cache'
+				}
+			}
+		},
+		{
+			urlPattern: "https://space-invaders.com/api/highscore/?uid=false",
+			handler: 'cacheFirst',
+			options: {
+				cache: {
+					name: 'SIAPI-highscores'
 				}
 			}
 		},
