fb095a286e91ddbd2254a903f8538d95e4a61c99
vision4 <romain.daniel@epitech.eu>
2019-10-01 17:45:25 +0200

    optimize for seo

--- serve.js
+++ serve.js
@@ -10,7 +10,7 @@ const prpl = require('prpl-server'),
 
 
 console.info(process.env.PORT || 5002);
-const images = (req, res) => {
+const images = (req, res, next) => {
     console.log(req.path);
     var options = {
         root: path.join(__dirname, `images/${req.path.split('/')[2]}`),
@@ -23,11 +23,22 @@ const images = (req, res) => {
     var fileName = req.params.name;
     res.sendFile(fileName, options, err => err ? next(err) : console.log('Sent:', fileName));
 };
+const txt = (req, res, next) => res.sendFile(req.path, {
+		root: __dirname,
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	}, err => err ? next(err) : console.log('Sent:', req.path));
 app.use(compression());
 app.set('view cache', true);
 app.use('/', httpsRedirect());
 app.get('/images/invaders/:name', images);
 app.get('/images/books/:name', images);
 app.get('/images/maps/:name', images);
+app.get('/robots.txt', txt);
+app.get('/humans.txt', txt);
+app.get('/sitemap.xml', txt);
 app.get('/*', prpl.makeHandler('./build/', config));
 app.listen(process.env.PORT || 5002);
